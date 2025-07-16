5a3e1d02c29903798e3b50e510c4effcd5290be7
dora-xiao <adoraboal@gmail.com>
2025-07-15 21:29:13 -0400

    Initial Commit

---
+++ Unlimited Spelling Bee/Unlimited_Spelling_BeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unlimited_Spelling_BeeApp.swift
+//  Unlimited Spelling Bee
+//
+//  Created by Dora Xiao on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Unlimited_Spelling_BeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
