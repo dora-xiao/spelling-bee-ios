ce52bc66c6433df9e421a834825f8e6bdd9d589a
dora-xiao <adoraboal@gmail.com>
2025-07-15 23:36:45 -0400

    Styled home page

---
+++ Unlimited Spelling Bee/Model/Util.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+import SwiftUI
+
+extension UIColor {
+  convenience init(red: Int, green: Int, blue: Int) {
+    assert(red >= 0 && red <= 255, "Invalid red component")
+    assert(green >= 0 && green <= 255, "Invalid green component")
+    assert(blue >= 0 && blue <= 255, "Invalid blue component")
+    
+    self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+  }
+  
+  convenience init(rgb: Int) {
+    self.init(
+      red: (rgb >> 16) & 0xFF,
+      green: (rgb >> 8) & 0xFF,
+      blue: rgb & 0xFF
+    )
+  }
+}
+
+extension Color {
+  static let customGrey = Color(UIColor(rgb: 0xE6E6E6))
+  static let customYellow = Color(UIColor(rgb: 0xF7DA20))
+}
--- Unlimited Spelling Bee/View/HomeView.swift
+++ Unlimited Spelling Bee/View/HomeView.swift
@@ -4,34 +4,62 @@ struct HomeView: View {
   @EnvironmentObject var appData : AppData
   
   var body: some View {
-    VStack(spacing: 20) {
-      Text("Spelling Bee")
-        .font(.largeTitle)
-        .padding()
-      
-      Button("Choose Puzzle") {
-        appData.currView = CurrView.datepicker
-        appData.prevViews.append(CurrView.home)
+    GeometryReader { geometry in
+      ZStack {
+        Color.customYellow
+          .ignoresSafeArea()
+        
+        VStack(spacing: 20) {
+          Image("bee_logo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: UIScreen.main.bounds.width * 0.5)
+            .padding([.bottom], 40)
+          
+          Button {
+            appData.currView = CurrView.datepicker
+            appData.prevViews.append(CurrView.home)
+          }
+        label: {
+          Text("Choose Puzzle")
+            .frame(width: UIScreen.main.bounds.width * 0.5)
+            .padding(6)
+        }
+        .buttonStyle(.borderedProminent)
+        .tint(Color.black)
+        .font(.title2)
+        .cornerRadius(50)
+          
+          Button {
+            appData.currView = CurrView.puzzle
+            appData.prevViews.append(CurrView.home)
+          } label: {
+            Text("Random")
+              .frame(width: UIScreen.main.bounds.width * 0.5)
+              .padding(6)
+          }
+          .buttonStyle(.borderedProminent)
+          .tint(Color.black)
+          .font(.title2)
+          .cornerRadius(50)
+          
+          Button {
+            appData.currView = CurrView.history
+            appData.prevViews.append(CurrView.home)
+          } label: {
+            Text("History")
+              .frame(width: UIScreen.main.bounds.width * 0.5)
+              .padding(6)
+          }
+          .buttonStyle(.borderedProminent)
+          .tint(Color.black)
+          .font(.title2)
+          .cornerRadius(50)
+          
+        }
+        .background(Color.clear)
       }
-      .buttonStyle(.borderedProminent)
-      .tint(Color.yellow)
-      .font(.title2)
-      
-      Button("Random") {
-        appData.currView = CurrView.puzzle
-        appData.prevViews.append(CurrView.home)
-      }
-      .buttonStyle(.borderedProminent)
-      .tint(Color.yellow)
-      .font(.title2)
-      
-      Button("History") {
-        appData.currView = CurrView.history
-        appData.prevViews.append(CurrView.home)
-      }
-      .buttonStyle(.borderedProminent)
-      .tint(Color.yellow)
-      .font(.title2)
+      .background(Color.clear)
     }
   }
 }
