f2557df1e77c8e29200d4d23a6960818beebef5e
dora-xiao <adoraboal@gmail.com>
2025-07-19 10:06:31 -0400

    Set up history back button

--- Unlimited Spelling Bee/View/HistoryView.swift
+++ Unlimited Spelling Bee/View/HistoryView.swift
@@ -8,12 +8,19 @@ struct HistoryView: View {
       Color.customWhite
         .ignoresSafeArea()
       
-      Image(systemName: "chevron.left")
-        .padding(20)
-        .bold()
-        .font(.title3)
-        .onTapGesture { appData.navigate(Views.home) }
-        .foregroundColor(Color.black)
+      HStack {
+        Image(systemName: "chevron.left")
+          .foregroundColor(Color.black)
+          .padding([.leading, .top, .bottom], 20)
+          .padding([.trailing], 10)
+          .bold()
+          .font(.title3)
+          .onTapGesture { appData.navigate(Views.home) }
+        Text("History")
+          .font(.title3)
+          .foregroundColor(Color.black)
+        Spacer()
+      }.padding([.bottom], 10)
       
       VStack() {
         Spacer()
